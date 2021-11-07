89896dceb565a366ed1ed5973068bcb2922a0a5c
JamesGabby <jlghfe36@gmail.com>
2021-11-07 18:36:41 +0000

    amendments on About section

--- src/about/about.component.ts
+++ src/about/about.component.ts
@@ -7,12 +7,12 @@ import { Component } from "@angular/core";
 })
 
 export class AboutComponent {
-  overview = `A versatile and hardworking developer with a Masters degree
-   in software engineering and a passion for computer science and philosophy. 
-   Currently seeking a junior or graduate JavaScript Developer role in Manchester
-    where I can put my skills and knowledge to good use. I'm very positive and have
-     a growth mindset, always finding continuous ways to learn, improve and advance
-      my technological and professional skill set.`;
+  overview = `A recent MSc software engineering graduate. I'm a versatile and hardworking 
+  developer with a passion for computer science and philosophy. 
+   Currently seeking a junior or graduate developer role in Manchester
+    where I can put my skills and knowledge to good use. I'm optimistic and have
+     a growth mindset; always finding continuous ways to learn, improve and advance
+      my technological and professional skills.`;
   languages = `Proficiency in: JavaScript, TypeScript, Java, Node, Angular and SQL.`;
   other = `Worked with: ReactJS, ExpressJS, AWS, Google Cloud, PostgreSQL, MySQL, NoSQL, MongoDB, Android, 
   JQuery, WebAssembly, Emscripten, Ajax, APIs (REST+SOAP), HTML5, SCSS, JSP, MVC.`;
