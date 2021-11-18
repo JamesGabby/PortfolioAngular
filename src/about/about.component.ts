5257bcf246fbf66cc7fd9ee399dbc6587abac8d7
JamesGabby <jlghfe36@gmail.com>
2021-11-18 23:26:08 +0000

    responsiveness, added to about

--- src/about/about.component.ts
+++ src/about/about.component.ts
@@ -14,10 +14,11 @@ export class AboutComponent {
   a growth mindset; always finding continuous ways to learn, improve and advance
   my technological and professional skills.`;
   languages = `The languages I'm proficient with are: JavaScript, TypeScript, Java, C++ and SQL.`;
-  frameworks = `The frameworks I'm familiar with are: Angular and Spring.`;
-  libraries = `The libraries I'm comfortable with are: Express, React and JQuery.`;
-  cloud = `The cloud providers I'm comfortable with are: Google Cloud Provider (GCP), Amazon Web Services (AWS)`;
-  databases = `The databases I know are: MySQL, any RDBM, MongoDB`;
+  frameworks = `Frameworks: Angular, Amplify and Spring.`;
+  runtimes = `Runtime Environments: Node.`
+  libraries = `Libraries: Express, React and JQuery.`;
+  cloud = `Cloud providers: Google Cloud Provider (GCP), Amazon Web Services (AWS)`;
+  databases = `Querying technologies: MySQL, any RDBMS, MongoDB, GraphQL, DynamoDB`;
   interests = `My interests include: Gaming, working out, coding, AI, philosophy, watching TV series and movies, 
                listening to music and playing some guitar.`;
   sent = 'For business enquiries you can reach me at: ';
