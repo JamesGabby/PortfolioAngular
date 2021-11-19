15131d002005937beb4336f5f34076572c69cee3
JamesGabby <jlghfe36@gmail.com>
2021-11-19 14:35:32 +0000

    added to experience

--- src/experience/experience.component.ts
+++ src/experience/experience.component.ts
@@ -6,4 +6,23 @@ import { Component } from "@angular/core";
   styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent {
+  info = `It is a requirement to effectively communicate with customers and members of the team over the course of each shift.
+  As a sales assistant it is necessary to deal with a variety of customers and ensure that communication and fulfilment of customer
+  queries are met to a high standard. Communicating with the department manager is important as they
+  have the information needed on what work is prioritised in order to maintain a standard of high customer satisfaction. The work that is done as a sales assistant revolves around customer satisfaction and making sure that each customer 
+  will be very happy with their shopping experience. It is up to the sales assistant to use best practice and meet this 
+  requirement as they are interacting directly with each customer and those interactions will greatly impact their shopping
+  experience.`;
+  preface = `Transferable skills that I have acquired and refined through the years of working here:`;
+  image = `https://1000logos.net/wp-content/uploads/2018/08/Morrisons-Logo.png`;
+  comm = `Communication is a universal skill that is crucial for any business. During my time at Morrisons I have developed this 
+  skill to a high level and I'm able to communicate effectively with a diverse range of people.`;
+  centric = `Another skill that is ubiquitous in the business world - Knowing your target audience and who you're aiming at.
+  Through my years at Morrisons I've come to understand the importance of centralising the work around the people who are 
+  using the product.`;
+  opt = ``;
+  team = ``;
+  init = ``;
+  time = ``;
+
 }
